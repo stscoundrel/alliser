e216cd944188096bfe7ecf09c770af30d5ee7a88
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2022-01-07 15:34:24 +0200

    CLI: add test cases

--- src/cli.ts
+++ src/cli.ts
@@ -4,16 +4,22 @@ import alliser from './index';
 
 const parseCommaSeparated = (input: string): string[] => input.split(',');
 
-const main = (args: string[]) => {
+export const main = (args: string[]): void => {
   if (args.length < 2) {
     console.error('Alliser expects two arguments: comma separated list of extensions (eg. .ts,.tsx and comma separated folders (eg. src,tests)');
     process.exit(1);
+  } else {
+    const extensions = parseCommaSeparated(args[0]);
+    const folders = parseCommaSeparated(args[1]);
+
+    alliser.validate(extensions, folders);
   }
-
-  const extensions = parseCommaSeparated(args[0]);
-  const folders = parseCommaSeparated(args[1]);
-
-  alliser.validate(extensions, folders);
 };
 
-main(process.argv.slice(2));
+if (process.env.NODE_ENV !== 'test') {
+  main(process.argv.slice(2));
+}
+
+export default {
+  main,
+};
