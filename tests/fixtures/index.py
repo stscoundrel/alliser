dd26bb8b0e086222cf9aa0cfa86881d7428e2742
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2022-01-29 18:29:56 +0200

    Fixtures: add dummy content

---
+++ tests/fixtures/index.py
@@ -0,0 +1,5 @@
+# For Balder the mead, a drink for the noble
+# I was forced to speak, now silence is my name
+
+# Hod shall guide him, shall be his bane
+# I was forced to speak, now silence is my name
